0360374e716a3d2f016124588b461c934c4c61b5
Vlad Savrasov <vsvsvs08@rambler.ru>
2024-06-10 21:02:22 +0300

    add swagger coverage

--- src/main/java/org/example/api/models/BuildType.java
+++ src/main/java/org/example/api/models/BuildType.java
@@ -1,5 +1,6 @@
 package org.example.api.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties
 public class BuildType {
     private String id;
     private String name ;
--- src/main/java/org/example/api/models/ServerAuthDettings/AuthModule.java
+++ src/main/java/org/example/api/models/ServerAuthDettings/AuthModule.java
@@ -1,5 +1,6 @@
 package org.example.api.models.ServerAuthDettings;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties
 class AuthModule {
     private String name;
     private Properties properties;
