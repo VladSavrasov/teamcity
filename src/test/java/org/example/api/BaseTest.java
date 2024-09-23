6433836b853daa19c0838ee5bc5d190d59227c87
Vlad Savrasov <vsvsvs08@rambler.ru>
2024-09-23 13:32:23 +0300

    add swagger coverage

--- src/test/java/org/example/api/BaseTest.java
+++ src/test/java/org/example/api/BaseTest.java
@@ -14,6 +14,7 @@ import org.testng.annotations.BeforeSuite;
 
 public class BaseTest {
     protected SoftAssertions softy;
+
     public TestDataStorage testDataStorage;
     public CheckedRequests checkedWithSuperUser = new CheckedRequests(Specifications.getSpec().superUserSpec());
     public UncheckedRequests uncheckedWithSuperUser = new UncheckedRequests(Specifications.getSpec().superUserSpec());
